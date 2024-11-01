60e991fcf9e2ea8efc1ad993258ffdfc7ec57f7e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-01 20:42:06 -0300

    programa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿ExibirTitulo();
+
+void ExibirTitulo()
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("----------------------");
+    Console.WriteLine("--- Par ou ímpar ---");
+    Console.WriteLine ("------------------\n");
+    Console.ResetColor();
+}
+
+
+Console.Write("Informe um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+bool ehPar = DefinirPar(numero);
+
+
+bool DefinirPar(int numero)
+{
+    bool ehPar = numero % 2 == 0;
+    return ehPar;
+}
+
+
+ExibirResultado();
+
+
+void ExibirResultado()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"O número {numero} é  um número {(ehPar ? "Par" : "Ímpar")}");
+    Console.ResetColor();
+}
